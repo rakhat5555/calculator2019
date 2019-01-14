67a748fd25ddd5718e835679a8f4031cb5e422de
kohichi horii and Rakhat Tutueva <rakhat@>
2019-01-14 14:05:20 -0800

    add BMI, degree liquids

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
-var add = function(number1, number2) {
+function add(number1, number2) {
   return number1 + number2;
+};
+
+function sub(number1, number2) {
+  return number1 - number2;
+};
+
+function multi(number1, number2) {
+  return number1 * number2;
+};
+
+function divi(number1, number2) {
+  return number1 / number2;
 };
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
+var result = divi(number1, number2);
 alert(result);
