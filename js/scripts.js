4ffe6c735a44692203294702612d2ffffaf50406
Nate Wessels and Rakhat Tutueva <rakhat@>
2019-01-16 13:10:06 -0800

    follow instructions, add multi, divide, subtract to page.

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,40 @@ function divi(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = divi(number1, number2);
-alert(result);
+
+
+$(document).ready(function(){
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#addOutput").text(result);
+  });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = sub(number1, number2);
+    $("#subtractOutput").text(result);
+});
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divi(number1, number2);
+    $("#divideOutput").text(result);
+
+  });
+
+  $("form#multi").submit(function(even) {
+    even.preventDefault();
+    var number1 = parseInt($("#multi1").val());
+    var number2 = parseInt($("#multi2").val());
+    var result = multi(number1, number2);
+    $("#multiOutput").text(result);
+
+
+  })
+
+});
